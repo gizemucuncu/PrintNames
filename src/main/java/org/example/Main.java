a78f28de3afb0e44ce1ee3d7c2651d4b31256e64
Gizem <gizemucuncu34@gmail.com>
2025-04-22 00:48:45 -0700

    Fixed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,27 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Geleneksel yöntemle liste elemanlarını yazdırma
         List<String> names = new ArrayList<>();
-
         names.add("Ahmet");
         names.add("Ayşe");
         names.add("Mehmet");
         names.add("Zeynep");
 
-        // Geleneksel yöntemle liste elemanlarını yazdırma
         for (String name : names) {
             System.out.println(name);
         }
 
+        System.out.println();
         // Method Reference yöntemle liste elemanlarını yazdırma
-        names.forEach(System.out::println);
+        List<String> namesNew =  Arrays.asList("Ahmet","Ayşe","Mehmet","Zeynep");
+        namesNew.forEach(System.out::println);
 
     }
 }
